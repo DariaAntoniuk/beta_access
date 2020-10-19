2c00d47085b3093344971262ad1bd93147aa1d21
Daria Antoniuk <daria@MacBook-Pro.local>
2020-10-19 20:45:54 +0300

    Add redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,23 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { accessKeyReducer } from './accessKey';
 
+const accessKeyConfig = {
+    key: 'accessKey',
+    storage,
+    whitelist: ['accessKey'],
+};
+
 const rootReducer = combineReducers({
-    accessKey: accessKeyReducer,
+    accessKey: persistReducer(accessKeyConfig, accessKeyReducer),
 });
 
 export const store = createStore(
     rootReducer,
     compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
+
+export const persistor = persistStore(store);
